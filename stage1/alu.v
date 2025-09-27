b40d6e1f97f687d0d895ed9b2eda898f705f409a
Manav2006 <68633145+BHSamuraii@users.noreply.github.com>
2025-09-27 23:02:20 +0100

    Update alu.v

--- stage1/alu.v
+++ stage1/alu.v
@@ -1,16 +1,17 @@
+// Carries out the operation 
 module alu (
-    input  [3:0] opcode,   // 4-bit operation
+    input  [3:0] opcode, // 4 bit opcode (instruction) - comes from cpu.v
     input  [7:0] acc,      // current accumulator value
-    input  [3:0] operand,  // 4-bit immediate value
+    input  [3:0] operand,  // 4-bit immediate
     output reg [7:0] result
 );
     always @(*) begin
         case (opcode)
-            4'b0000: result = acc + operand;   // ADD (directly to accumulator)
-            4'b0001: result = acc - operand;   // SUB (directly to accumulator)
-            4'b0010: result = acc & operand;   // AND (bitwise)
-            4'b0011: result = acc | operand;   // OR (bitwise)
-            default: result = acc;
+            4'b0000: result = acc + operand;   // ADDI
+            4'b0001: result = acc - operand;   // SUBI
+            4'b0010: result = acc & operand;   // AND
+            4'b0011: result = acc | operand;   // OR
+            default: result = acc; // If opcode is anything else (apart from JMP as well), do nothing
         endcase
     end
 endmodule
