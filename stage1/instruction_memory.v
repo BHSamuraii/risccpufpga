1bebc627c5b131a63261e7919c1885913853f8cc
Manav2006 <68633145+BHSamuraii@users.noreply.github.com>
2025-09-27 23:01:49 +0100

    Update instruction_memory.v

--- stage1/instruction_memory.v
+++ stage1/instruction_memory.v
@@ -1,12 +1,13 @@
+// File used for storing all the instructions - it fetches from prog.mem and keeps in its own memory (rom)
 module instruction_memory (
-    input  [7:0] addr,
-    output [7:0] instr
+    input  wire [7:0] addr,
+    output wire [7:0] instr
 );
-    reg [7:0] rom [0:255];
+    reg [7:0] rom [0:255]; // Creates a register file with 256 8 bit locations
 
     initial begin
-        $readmemb("prog.mem", rom);
+        $readmemb("prog.mem", rom); // Loads entire prog.mem file into local ROM
     end
 
-    assign instr = rom[addr];
+    assign instr = rom[addr]; // update the instruction to match the PC value (each time it will obvs point to a new one)
 endmodule
