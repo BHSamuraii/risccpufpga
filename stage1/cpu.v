0aee9a14d3e24d96daebb81913978b47a3d8386a
Manav2006 <68633145+BHSamuraii@users.noreply.github.com>
2025-09-28 22:51:11 +0100

    Update cpu.v

--- stage1/cpu.v
+++ stage1/cpu.v
@@ -9,18 +9,19 @@ module cpu (
     wire [7:0] instr;   // actual 8 bit instruction (fetched from prog.mem via instruction_memory file)
 
     wire [3:0] opcode = instr[7:4];   // opcode for instruction (add,sub etc)
-    wire [3:0] operand = instr[3:0];  // operand for instruction (data/number)
-    wire [7:0] alu_out;
+    wire [3:0] operand = instr[3:0];  // operand for instruction (data / address in stage 2)
+    wire [7:0] alu_out; // output of ALU module (assigned to ACC later on)
 
     instruction_memory im ( // fetch the actual instruction from rom in instruction_memory.v
         .addr(pc), // connects the addr input port on instruction_memory.v file to the pc port (local to this file)
         .instr(instr) // the output (actual instruction) that the instruction_memory.v file gives us for the PC addr we gave it
     );
 
+    // Instantiate ALU module
     alu alu1 ( // execute instruction (will obvs decode to check which operation needs to be done and then will output the result)
-        .opcode(opcode),   // input for alu.v file
-        .acc(acc),         // input for alu.v file
-        .operand(operand), // input for alu.v file
+        .opcode(opcode),   // input for alu.v file (4 bit)
+        .acc(acc),         // input for alu.v file (8 bit)
+        .operand(operand), // input for alu.v file (4 bit)
         .result(alu_out)   // alu.v file outputs result which is acc value following execution of instruction (this is then mapped to alu_out)
     );
 
